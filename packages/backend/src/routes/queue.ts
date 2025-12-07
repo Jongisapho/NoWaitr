2dbecf30d1efaa4828963f708cfa8e9afeddc2e4
Jongisapho Ndeya <u22625926@tuks.co.za>
2025-12-07 14:23:04 +0200

    Removed Queue routes

--- packages/backend/src/routes/queue.ts
+++ packages/backend/src/routes/queue.ts
@@ -3,10 +3,4 @@ import * as controller from "../controllers/queueController";
 
 const router = Router();
 
-router.post("/", controller.createQueue);
-router.get("/:id", controller.getQueue);
-router.post("/:id/join", controller.joinQueue);
-router.post("/:id/next", controller.serveNext);
-router.get("/:id/items", controller.listItems);
-
 export default router;
